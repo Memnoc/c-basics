eb122a86f9543a1eed9ef3fb4346dbf78c4fa9d5
memnoc <memnoc@localhost.localdomain>
2024-01-14 20:47:04 +0000

    constants

---
+++ Constants/Constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+  // constant = fixed value that cannot be altered by the program during its
+  // execution
+
+  const float PI = 31.14159; // capital as convention
+
+  // pi = 420.69; // this would give an error
+
+  printf("%f\n", PI);
+  return 0;
+}
