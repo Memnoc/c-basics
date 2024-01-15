2479725f5ac7bee7ff991e86f177fe59bb5004ec
memnoc <memnoc@localhost.localdomain>
2024-01-15 06:22:22 +0000

    program to calculate circumference

---
+++ circ-calc/CircCalculator.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+
+  const double PI = 3.14159;
+  double radius;
+  double circumference;
+  double area;
+
+  printf("\nEnter the radius of a circle: ");
+  scanf("%lf", &radius);
+
+  circumference = 2 * PI * radius;
+  area = PI * radius * radius;
+
+  printf("circumference: %.0lf\n", circumference);
+  printf("area: %.0lf\n", area);
+
+  return 0;
+}
