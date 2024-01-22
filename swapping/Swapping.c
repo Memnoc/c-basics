b3c4e155e5e186de860fb01859d29acb5e31e2a4
Memnoc <smartdroidesign@gmail.com>
2024-01-22 22:01:59 +0000

    swapping strings

--- swapping/Swapping.c
+++ swapping/Swapping.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
@@ -12,6 +13,20 @@ int main() {
 
   printf("x = %c\n", x);
   printf("y = %c\n", y);
+
+  // HACK: for strings it works a little differently
+  //  you must use the copy function
+
+  char lemonade[15] = "Lemonade";
+  char water[15] = "water";
+  char temp_string[15];
+
+  strcpy(temp_string, lemonade);
+  strcpy(lemonade, water);
+  strcpy(water, temp_string);
+
+  printf("Lemonade = %s\n", lemonade);
+  printf("Water = %s\n", water);
 
   return 0;
 }
