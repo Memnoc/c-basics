3f41debe694418d93bfdacc8982e8b92451b8fb1
Memnoc <smartdroidesign@gmail.com>
2024-01-22 21:23:38 +0000

    array of strings

--- 2D-array/2D.c
+++ 2D-array/2D.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
-// INFO: an arrays where each element is an entire array
+// INFO: an array where each element is an entire array
 //  comes in handy with matrix and grids
 // HACK: first [2] is the amount of arrays
 // second [3] is the size of all the arrays
@@ -18,6 +19,19 @@ int main() {
       printf("%d ", numbers[i][j]);
     }
     printf("\n");
+  }
+
+  // INFO: string arrays work a little differently
+  //  first, you cannot change the values direclty - this because
+  //  the size of the array is fixed, this means you have to make a copy
+  //  to change something, so a new array
+
+  char cars[][10] = {"Mustang", "Corvette", "Camaro"};
+
+  strcpy(cars[0], "Tesla");
+
+  for (int i = 0; i < sizeof(cars) / sizeof(cars[0]); i++) {
+    printf("%s\n", cars[i]);
   }
 
   return 0;
