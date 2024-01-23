6deebf88b2ebf210a87c5a4e91a50d294062cf7c
Memnoc <smartdroidesign@gmail.com>
2024-01-23 06:31:36 +0000

    typedef

---
+++ typedef/Type.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+// NOTE: it's a reserved keyword to give an existing datatype a "nickname"
+//  from char user1[25] = "Name"
+//  to typedef char user[25]; -> user user1 = "Name";
+
+typedef char user[25];
+
+/* HACK: used a lot with structs
+ */
+
+typedef struct {
+  char name[25];
+  int score;
+  int id;
+} User;
+
+int main() {
+
+  user user1 = "Bro";
+
+  // struct User user2 = {"Memnoc", 10, 1};
+  /* HACK: don't have to type the struct keyword
+   */
+  User user2 = {"Memnoc", 10, 1};
+  User user3 = {"Brah", 15, 8};
+
+  printf("%s\n", user2.name);
+  printf("%d\n", user2.score);
+  printf("%d\n", user2.id);
+
+  printf("%s\n", user3.name);
+  printf("%d\n", user3.score);
+  printf("%d\n", user3.id);
+
+  return 0;
+}
