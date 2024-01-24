530990cbf321e6031b72fce400c3bd03bb5ef4a9
Memnoc <smartdroidesign@gmail.com>
2024-01-24 07:44:34 +0000

    quiz game

---
+++ quiz-game/Quiz.c
@@ -0,0 +1,60 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main() {
+
+  char questions[][100] = {"1. What year did the language C debut?: ",
+                           "2. Who is credited with creating C?: ",
+                           "3. What is the predecessor of C?: "};
+
+  char options[][100] = {"A. 1969",
+                         "B. 1972",
+                         "C. 1975",
+                         "D. 1999",
+                         "A. Dennis Ritchie",
+                         "B. Nikola Tesla",
+                         "C. John Carmak",
+                         "D. Doc Brown",
+                         "A. Objective C",
+                         "B. B",
+                         "C. C++",
+                         "D. C#"};
+  char answers[3] = {'B', 'A', 'B'};
+  int number_of_questions = sizeof(questions) / sizeof(questions[0]);
+
+  char guess;
+  int score;
+
+  printf("***************QUIZ GAME***************\n");
+
+  for (int i = 0; i < number_of_questions; i++) {
+    printf("***************************************\n");
+    printf("%s\n", questions[i]);
+    printf("***************************************\n");
+
+    for (int j = (i * 4); j < (i * 4) + 4; j++) {
+      printf("%s\n", options[j]);
+    }
+    printf("guess: ");
+    scanf(" %c", &guess);
+    /* HACK: clear the input buffer
+     */
+    while ((getchar()) != '\n')
+      ;
+    guess = toupper(guess);
+
+    if (guess == answers[i]) {
+      printf("CORRECT!\n");
+      score++;
+
+    } else {
+      printf("WRONG!\n");
+    }
+  }
+
+  printf("***************************************\n");
+  printf("FINAL SCORE: %d/%d\n", score, number_of_questions);
+  printf("***************************************\n");
+
+  return 0;
+}
