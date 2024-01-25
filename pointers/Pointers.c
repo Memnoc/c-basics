ae4635ffbe4f03481f59b5f91e9f6ac37f8c3d5d
Memnoc <smartdroidesign@gmail.com>
2024-01-25 21:07:50 +0000

    pointers

---
+++ pointers/Pointers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/*
+ * INFO: a "variable-like" reference that holds a memory address to another
+ * variable as value to another variable, array, etc. some tasks are performed
+ * more easily with pointers
+ * * = indirection operator (value at address)
+ */
+
+/*
+ * HACK: it is good practice to assign NULL if declaring a pointer
+ * int *pAge = NULL;
+ */
+
+void print_age(int *pAge) {
+  printf("You are %d years old\n", *pAge); // dereference
+};
+
+int main() {
+
+  int age = 21;
+  int *pAge = &age;
+
+  printf("address of age: %p\n", &age);
+  printf("address of pAge: %p\n", pAge);
+
+  printf("size of age: %lu bytes\n", sizeof(age));
+  printf("size of pAge: %lu bytes\n", sizeof(pAge));
+
+  print_age(pAge);
+
+  return 0;
+}
